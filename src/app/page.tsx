c11fa8e2eb0054f5c466129a7d6fcc75e3490156
Abhiyan Paudel <abhiyanpaudel@Abhiyans-MacBook-Air.local>
2025-07-02 22:44:19 +0545

    Version 1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,82 @@
-import Image from "next/image";
+'use client'
+import { Button } from '@/components/ui/button';
+import React, { useState } from 'react'
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+const wordList = ['cat', 'dog', 'fish', 'lion', 'tiger', 'elephant', 'giraffe', 'zebra'];
+const guessWord = wordList[Math.floor(Math.random() * wordList.length)]
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+const Hangman = () => {
+    const keyboard = [
+        ['A', 'B', 'C', 'D', 'E'],
+        ['F', 'G', 'H', 'I', 'J'],
+        ['K', 'L', 'M', 'N', 'O'],
+        ['P', 'Q', 'R', 'S', 'T'],
+        ['U', 'V', 'W', 'X', 'Y'],
+        ['Z']
+    ];
+
+    const [step, setStep] = useState(1)
+    const [guesssedword, setGuessedWord] = useState([])
+    const mainWord = guessWord.toUpperCase()
+
+    const handleGuess = (val) => {
+        const isIncluded = (mainWord.split('')).includes(val)
+        if (!isIncluded) {
+            setStep(step + 1)
+        }
+
+        if (isIncluded) {
+            setGuessedWord([...guesssedword, val])
+        }
+    }
+
+    return (
+        <div className='m-5 bg-gray-200 w-screen h-screen'>
+          <div className='m-2 p-2 border border-black rounded-[20px] w-max'>
+            <span className='text-4xl font-bold'>Hangman</span><br /><br />
+            <span className='italic m-4 '>Theme: Animals</span>
+            </div>
+
+            {step == 7 && (
+                <div className='bg-black w-50 m-2 p-2'>
+                    <span className='text-white'>YOU LOST THE GAME!!! <span className='font-bold m-3'>The word was {mainWord}</span></span>
+                </div>)}
+            <div className='items-center absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2'>
+                 <div className='border border-black rounded-[25px] p-2 m-3 bg-purple-200 w-max '>
+                <img src={`/hang_${step}.gif`} alt="Default" className='m-3' />
+
+                {JSON.stringify(guesssedword) === JSON.stringify(mainWord.split('')) && //JSON.stringify converts array into string as direct array comparision was not possible here
+                    (<div className='bg-black w-100 m-2 p-2'>
+                        <span className='text-white'>CONGRATULATIONS!!! YOU GUESSED THE WORD CORRECTLY.</span>
+                    </div>)}
+
+               
+                    {mainWord.split('').map((item, idx) => {
+                        return (<span className='m-2' key={idx}>
+                            {guesssedword.includes(item) ? item : '___'} </span>)
+                    })}
+                </div>
+
+                <div className='border border-black m-2 p-2 rounded-[25px] bg-red-200 w-80'>
+                    {step < 7 && !(JSON.stringify(guesssedword) === JSON.stringify(mainWord.split(''))) && keyboard.map((items, id) => {
+                        return (<div className='flex m-2' key={id}>
+                            {items.map((itm, idx) => {
+                                return (<div key={idx}>
+                                    <Button className='m-2' onClick={() => handleGuess(itm)}>{itm}</Button>
+                                </div>)
+                            })}
+                        </div>)
+                    })}
+                </div>
+            </div>
+
+            <div className='bg-green-200 absolute bottom-0 w-screen font-bold text-center p-1'>
+                Created By Abhiyan Paudel
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+
+    )
 }
+
+export default Hangman
